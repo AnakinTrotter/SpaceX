a07297ea6036b30b59194ad19a58dc7e97b05c34
Zachary Mayhew <mayhew.zachary2003@gmail.com>
2016-01-28 18:03:02 -0800

    added if sign clicked with stick

--- src/me/reddev/spacex/Craft.java
+++ src/me/reddev/spacex/Craft.java
@@ -1,11 +1,19 @@
 package me.reddev.spacex;
 
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+
 /**
  * An object that is the craft
  * @author Zachary Mayhew
  * @author Anakin Trotter
  */
 public class Craft {
+
+    private Player pilot;
+
+    private ArrayList<Player> passengers;
 
     /**
      * The top ppints x, y, and z
@@ -16,5 +24,7 @@ public class Craft {
      * The Bottom points x, y and z
      */
     private int x1, y1, z1;
+
+
 
 }
--- src/me/reddev/spacex/SpaceX.java
+++ src/me/reddev/spacex/SpaceX.java
@@ -1,7 +1,16 @@
 package me.reddev.spacex;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Fireball;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import com.onarandombox.MultiverseCore.*;
@@ -34,10 +43,29 @@ public class SpaceX extends JavaPlugin {
         return false;
     }
 
+    @EventHandler(priority=EventPriority.HIGH)
+    public void onPlayerInteract(PlayerInteractEvent event){
+        Player p = event.getPlayer();
+        if(p.getItemInHand().getType() == Material.STICK){
+            if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+                Block block = event.getClickedBlock();
+                if (block.getType() == Material.SIGN) {
+                    Sign sign = (Sign) block;
+                    if (sign.getLine(0).equalsIgnoreCase("spaceship")) {
+                        p.sendMessage("this will pilot spaceship");
+                    }
+                }
+            }
+        }
+    }
+
+
     /**
      * Called on player move event.  Avoid major calculations here.  Only do things if they are through an if.
+     * priority is normal because it also works if they are above 250 blocks.
      * @param m player event
      */
+    @EventHandler(priority= EventPriority.NORMAL)
     public void onMove(PlayerMoveEvent m){
         // Detects player height and will later move them to space world.
         if(m.getPlayer().getLocation().getBlockY()>=250){
